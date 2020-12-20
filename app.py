88b193a9c628760cb57dd9df13219ca6e106a7f1
Subhasish Kumar Behura <behuracool@gmail.com>
2020-12-20 19:05:06 +0530

    app.py Changed

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def predictRoute():
     result = client.classifier.prediction()
     return jsonify(result)
 
-if __name__=='__main__':
-    app.run(debug=True)
+# if __name__=='__main__':
+#     app.run(debug=True)
